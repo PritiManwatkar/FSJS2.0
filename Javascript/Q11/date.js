82384e9493adf0c4d57a5fbd70c3aeeea5d14bdd
Priti Manwatkar <sonpethkarpriti@gmail.com>
2023-03-14 18:59:15 +0530

    Signed-off-by: Priti Manwatkar <sonpethkarpriti@gmail.com>

--- Javascript/Q11/date.js
+++ Javascript/Q11/date.js
@@ -10,11 +10,11 @@
 const currentdate=new Date();
 console.clear();
 console.log(`Current year is:${currentdate.getFullYear()}`);
-console.log(`Current month is:${currentdate.getMonth()}`);
-console.log(`Current date is:${currentdate.getDate()}`);
+console.log(`Current month is:${(currentdate.getMonth()+1).toString().padStart(2,'0')}`);
+console.log(`Current date is:${currentdate.getDate().toString().padStart(2,'0')}`);
 console.log(`Current day is:${currentdate.getDay()}`);
-console.log(`Current hours are:${currentdate.getHours()}`);
-console.log(`Current minutes are:${currentdate.getMinutes()}`);
+console.log(`Current hours are:${currentdate.getHours().toString().padStart(2,'0')}`);
+console.log(`Current minutes are:${currentdate.getMinutes().toString().padStart(2,'0')}`);
 console.log(`Numbers of seconds elapsed from January 1, 1970 to now are:${Date.now()/1000}`);
 console.log(`Numbers of seconds elapsed from January 1, 1970 to now are:${(currentdate.getTime()+(currentdate.getTimezoneOffset()*60*1000))/1000}`);
 
