82aa9d72d179597d567899869ed0ec1a419d111d
Priti Manwatkar <sonpethkarpriti@gmail.com>
2023-04-06 14:34:54 +0530

    readme updated
    Signed-off-by: Priti Manwatkar <sonpethkarpriti@gmail.com>

--- Javascript/Q14/evenodd.js
+++ Javascript/Q14/evenodd.js
@@ -1,5 +1,6 @@
 /*Q14. Even numbers are divisible by 2 and the remainder is zero. 
-How do you check, if a number is even or not using JavaScript? Create a program which checks that the given number is even or odd.*/
+How do you check, if a number is even or not using JavaScript? Create a program which checks that the given number 
+is even or odd.*/
 let num=window.prompt("Enter any number:");
 let validnum=parseInt(num);
 if(validnum.toString().length==num.length && num%2==0)//check if interger and even
