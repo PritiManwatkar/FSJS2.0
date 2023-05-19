20dce765eb4a27efbfaad6861b0121c8a1086160
Priti Vaibhav Manwatkar <sonpethkarpriti@gmail.com>
2023-05-19 13:56:57 +0530

    Signed-off-by: Priti Vaibhav Manwatkar <sonpethkarpriti@gmail.com>

--- React/router/src/index.js
+++ React/router/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'
 import App from './App';
 import Home from './Home';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
